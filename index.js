868d765297df92b80b385a24535900a8dd290650
jjurman <jjurman@constantcontact.com>
2018-01-26 21:31:08 -0500

    Clean up index.js logic

--- index.js
+++ index.js
@@ -1,58 +1,49 @@
-const document = typeof window !== 'undefined'
-  ? window.document
-  : require('domino').createWindow().document
+const document = typeof window !== 'undefined' ? window.document : require('domino').createWindow().document
 
 const appendChild = require('./appendChild')
 
 const COMMENT_TAG = '!--'
 
-const belitCreateElement = (namespace) => (tag, props, children) => {
-  let el
-  let ns = namespace
+// filters for attributes
+const isNotXMLNSprop = prop => !/^xmlns($|:)/i.test(prop)
+const containsOwnProp = props => prop => props.hasOwnProperty(prop)
 
+// map to objects so we know their value
+const toObjectList = props => prop => ({key:prop, value:props[prop]})
+
+// transformations for attributes
+const normalizeClassName = prop => prop.key.toLowerCase() === 'classname' ? ({key:'class', value:prop.value}) : prop
+const htmlForToFor = prop => prop.key === 'htmlFor' ? ({key:'for', value:prop.value}) : prop
+
+// handlers that can be filtered on
+// if something gets processed, we return false (using ![])
+// otherwise, it returns true, indicating that this thing needs to be processed
+const handleOnAttrSetter = element => prop => prop.key.slice(0, 2) === 'on' ? ![element[prop.key] = prop.value] : true
+const handleNamespaceAttrSetter = (element, namespace) => prop => namespace ? ![element.setAttributeNS(null, prop.key, prop.value)] : true
+const handleAttrSetter = element => prop => element.setAttribute(prop.key, prop.value)
+
+const belitCreateElement = (namespace) => (tag, props, children) => {
   // if the tag is a comment
   if (tag === COMMENT_TAG) {
     return document.createComment(props.comment)
   }
 
   // Create the element
-  if (ns) {
-    el = document.createElementNS(ns, tag)
-  } else {
-    el = document.createElement(tag)
-  }
+  const element = namespace ? document.createElementNS(namespace, tag) : document.createElement(tag)
 
-  // Create the properties
-  for (let p in props) {
-    if (props.hasOwnProperty(p)) {
-      let key = p.toLowerCase()
-      const val = props[p]
-      // Normalize className
-      if (key === 'classname') {
-        key = 'class'
-        p = 'class'
-      }
-      // The for attribute gets transformed to htmlFor, but we just set as for
-      if (p === 'htmlFor') {
-        p = 'for'
-      }
-      // If a property prefers being set directly vs setAttribute
-      if (key.slice(0, 2) === 'on') {
-        el[p] = val
-      } else if (/^xmlns($|:)/i.test(p)) {
-        // skip xmlns definitions
-      } else {
-        if (ns) {
-            el.setAttributeNS(null, p, val)
-        } else {
-          el.setAttribute(p, val)
-        }
-      }
-    }
-  }
+  // Attach the properties
+  Object.keys(props)
+    .filter(isNotXMLNSprop)
+    .filter(containsOwnProp(props))
+    .map(toObjectList(props))
+    .map(normalizeClassName)
+    .map(htmlForToFor)
+    .filter(handleOnAttrSetter(element))
+    .filter(handleNamespaceAttrSetter(element, namespace))
+    .filter(handleAttrSetter(element))
 
-  appendChild(el, children)
-  return el
+  appendChild(element, children)
+  return element
 }
 
 module.exports = belitCreateElement
